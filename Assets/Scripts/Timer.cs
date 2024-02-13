6ab407037e74255fa746f2aa29251d4feec228a6
Swoogity <158215145+Swoogity@users.noreply.github.com>
2024-02-13 12:15:50 -0500

    final version

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.UI;
+//using Unity.UI;
 using TMPro;
 using Unity.VisualScripting;
 public class MusicBox : MonoBehaviour
